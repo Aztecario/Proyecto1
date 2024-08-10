393946f0445297bc3d08e43e0c4293f37298ce6c
Jhony <alexdergamer008@gmail.com>
2024-08-10 17:15:40 -0500

    coment2

---
+++ php/conexion.php
@@ -0,0 +1,35 @@
+<?php
+
+#Sinatsis de PHP
+#Tipos de variables
+
+#Enteros
+$entero = 10;
+
+
+#Flotantes
+$decimal = 10.5;
+
+#Cadenas
+$cadena ="Hola Mundo";
+
+#Booleanos
+$verdadero = true;
+$falso = false;
+
+#Ciclos
+#ciclo for
+
+for($i = 0; $i <10; i++){
+    echo $i ."<br>";
+}
+
+#Cilo while
+ $i = 0;
+ while ($i < 10){
+    echo $i . "<br>"
+ }
+
+ #Ciclo do while
+
+?>
